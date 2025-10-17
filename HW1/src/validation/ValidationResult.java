0c45d5c40b9f603dcec00abb7655d1e0203fe601
dresuper <dmdecuire@gmail.com>
2025-10-17 16:37:19 -0700

    Delete HW1 directory

--- HW1/src/validation/ValidationResult.java
+++
@@ -1,9 +0,0 @@
-package validation;
-
-public record ValidationResult(boolean valid, String message) {
-	// Successful result with no message
-	public static ValidationResult ok() { return new ValidationResult(true, ""); }
-	// Create a failed result with an error message
-	public static ValidationResult error(String msg) { return new ValidationResult(false, msg); }
-
-}
